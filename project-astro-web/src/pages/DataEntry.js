2751c770ee4c793e78de0090b1e5ffd9ccd90a98
tarun trivedi <tarundtrivedi@gmail.com>
2020-12-24 20:44:21 +0530

    drak table for bhav and planet added

--- project-astro-web/src/pages/DataEntry.js
+++ project-astro-web/src/pages/DataEntry.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Paper, Grid } from "@material-ui/core";
 import InputMask from "react-input-mask";
 import Select from "react-select";
-import MaterialTable from 'material-table'
+import MaterialTable from "material-table";
 import "./pages.scss";
 
 const Rashis = [
@@ -47,23 +47,29 @@ const Bhavas = [
 ];
 
 const customStyles = {
-  control: base => ({
+  control: (base) => ({
     ...base,
     height: 35,
     minHeight: 35,
-    width:200
-  })
+    width: 200,
+  }),
 };
 
 export class DataEntry extends Component {
   state = {
     planetDataArray: [],
     bhavaDataArray: [],
+    testP: [108, 208, 165, 117, 222, 104, 330, 186, 6],
+    testB: [77, 103, 130, 156, 190, 223, 257, 283, 310, 336, 10, 43],
+    drakBhavaData: [],
+    drakPlanetData:[]
   };
 
   componentDidMount = () => {
     this.initBhavas();
     this.initPlanets();
+    this.createDrakBalForBhavas();
+    this.createDrakBalForPlanets();
   };
 
   initPlanets = () => {
@@ -72,7 +78,7 @@ export class DataEntry extends Component {
         planetDataArray: [
           ...prevState.planetDataArray, // use prevState
           {
-            graha:item.label,
+            graha: item.label,
             finalData: null,
             rashiResult: 0,
             anshResult: 0,
@@ -87,8 +93,8 @@ export class DataEntry extends Component {
       this.setState((prevState) => ({
         bhavaDataArray: [
           ...prevState.bhavaDataArray, // use prevState
-          { 
-            bhava:item.label,
+          {
+            bhava: item.label,
             finalData: null,
             rashiResult: 0,
             anshResult: 0,
@@ -141,11 +147,17 @@ export class DataEntry extends Component {
           let finalData =
             this.state.planetDataArray[key].rashiResult +
             this.state.planetDataArray[key].anshResult;
-          this.setState((prevState) => ({
-            planetDataArray: prevState.planetDataArray.map((obj, index) =>
-              index === key ? Object.assign(obj, { finalData: finalData }) : obj
-            ),
-          }));
+          this.setState(
+            (prevState) => ({
+              planetDataArray: prevState.planetDataArray.map((obj, index) =>
+                index === key
+                  ? Object.assign(obj, { finalData: finalData })
+                  : obj
+              ),
+            }),
+            () => {
+            }
+          );
         }
       );
     }
@@ -195,15 +207,90 @@ export class DataEntry extends Component {
           let finalData =
             this.state.bhavaDataArray[key].rashiResult +
             this.state.bhavaDataArray[key].anshResult;
-          this.setState((prevState) => ({
-            bhavaDataArray: prevState.bhavaDataArray.map((obj, index) =>
-              index === key ? Object.assign(obj, { finalData: finalData }) : obj
-            ),
-          }));
+          this.setState(
+            (prevState) => ({
+              bhavaDataArray: prevState.bhavaDataArray.map((obj, index) =>
+                index === key
+                  ? Object.assign(obj, { finalData: finalData })
+                  : obj
+              ),
+            }),
+            () => {
+            }
+          );
         }
       );
     }
     console.log(this.state.bhavaDataArray);
+  };
+
+  createDrakBalForBhavas = () => {
+    // if(this.state.bhavaDataArray.every(bhav => bhav.finalData !== null) &&  this.state.planetDataArray.every(planet => planet.finalData !== null)){
+    //   console.log("drak bal started")
+    // }
+    let drakData = this.state.drakBhavaData;
+    this.state.testP.forEach((planet, pIndex) => {
+      let result;
+      let arr = [];
+      this.state.testB.forEach((bhav, bIndex) => {
+        if (pIndex !== 7 && pIndex !== 8) {
+          let value = bhav - planet;
+          if (value < 0) {
+            value = value + 360;
+          }
+          result = {
+            key: bIndex,
+            value: value,
+          };
+          
+          arr.push(result);
+        }
+        else{
+          result={
+            key:bIndex,
+            value:"N/A"
+          }
+          arr.push(result);
+        }
+      });
+      drakData[pIndex]=arr
+    });
+    console.log(drakData)
+  };
+  createDrakBalForPlanets = () => {
+    // if(this.state.bhavaDataArray.every(bhav => bhav.finalData !== null) &&  this.state.planetDataArray.every(planet => planet.finalData !== null)){
+    //   console.log("drak bal started")
+    // }
+    let drakData = this.state.drakPlanetData;
+    this.state.testP.forEach((planet, pIndex) => {
+      let result;
+      let arr = [];
+        this.state.testP.forEach((graha, bIndex) => {
+          if (pIndex !== 7 && pIndex !== 8 && bIndex!==7 && bIndex!==8) {
+            let value = graha - planet;
+            if (value < 0) {
+              value = value + 360;
+            }
+            result = {
+              key: bIndex,
+              value: value,
+            };
+            
+            arr.push(result);
+          }
+          else{
+            result={
+              key:bIndex,
+              value:"N/A"
+            }
+            arr.push(result);
+          }
+        });
+      
+      
+      drakData[pIndex]=arr
+    });
+    console.log(drakData)
   };
 
   planetInputCreator = (graha) => {
@@ -212,7 +299,7 @@ export class DataEntry extends Component {
         <span className="content-title">{graha.label} :</span>
         <div className="content-select">
           <Select
-          styles={customStyles}
+            styles={customStyles}
             options={Rashis}
             onChange={(e) => {
               this.handleInput(e, "rashi", graha.key);
@@ -269,25 +356,26 @@ export class DataEntry extends Component {
           <Grid item xs={6}>
             <Paper className="paper-container">
               <div className="paper-heading">ग्रह स्पष्ट</div>
-              <div className="paper-contents">{planetInputs}</div>
-              <div className="paper-table">
-              <MaterialTable
-          columns={[
-            { title: 'ग्रह', field: 'graha' },
-            { title: 'राशि', field: 'rashiResult' },
-            { title: 'अंश ', field: 'anshResult', },
-            { title: 'कुल ग्रहांश', field: 'finalData' }
-          ]}
-          data={this.state.planetDataArray}
-          title="ग्रहांश सारणी"
-          options={{
-            pageSize:9,
-            pageSizeOptions: [],
-            search:false,
-            paging:false
-          }}
-        />
-      
+              <div className="paper-contents" style={{ paddingBottom: 156 }}>
+                {planetInputs}
+              </div>
+              <div className="paper-table" style={{ paddingBottom: 165 }}>
+                <MaterialTable
+                  columns={[
+                    { title: "ग्रह", field: "graha" },
+                    { title: "राशि", field: "rashiResult" },
+                    { title: "अंश ", field: "anshResult" },
+                    { title: "कुल ग्रहांश", field: "finalData" },
+                  ]}
+                  data={this.state.planetDataArray}
+                  title="ग्रहांश सारणी"
+                  options={{
+                    pageSize: 9,
+                    pageSizeOptions: [],
+                    search: false,
+                    paging: false,
+                  }}
+                />
               </div>
             </Paper>
           </Grid>
@@ -296,26 +384,87 @@ export class DataEntry extends Component {
               <div className="paper-heading">भाव स्पष्ट</div>
               <div className="paper-contents">{bhavaInputs}</div>
               <div className="paper-table">
-              <MaterialTable
-          columns={[
-            { title: 'भाव', field: 'bhava' },
-            { title: 'राशि', field: 'rashiResult' },
-            { title: 'अंश ', field: 'anshResult', },
-            { title: 'कुल भावांश ', field: 'finalData' }
-          ]}
-          data={this.state.bhavaDataArray}
-          title="भावांश सारणी"
-          options={{
-            pageSize:12,
-            pageSizeOptions: [],
-            search:false,
-            paging:false
-          }}
-        />
-      
+                <MaterialTable
+                  columns={[
+                    { title: "भाव", field: "bhava" },
+                    { title: "राशि", field: "rashiResult" },
+                    { title: "अंश ", field: "anshResult" },
+                    { title: "कुल भावांश ", field: "finalData" },
+                  ]}
+                  data={this.state.bhavaDataArray}
+                  title="भावांश सारणी"
+                  options={{
+                    pageSize: 12,
+                    pageSizeOptions: [],
+                    search: false,
+                    paging: false,
+                  }}
+                />
               </div>
             </Paper>
           </Grid>
+        <Grid item xs ={12}>
+        <Paper className="paper-container">
+              <div className="paper-heading">भावोपरि ग्रहाणां  दृष्टयः </div>
+              <div className="paper-table">
+                <MaterialTable
+                  columns={[
+                    { title: "भाव 1", render: rowData => <span>{rowData[0].value}</span> },
+                    { title: "भाव 2", render: rowData => <span>{rowData[1].value}</span> },
+                    { title: "भाव 3", render: rowData => <span>{rowData[2].value}</span> },
+                    { title: "भाव 4", render: rowData => <span>{rowData[3].value}</span> },
+                    { title: "भाव 5", render: rowData => <span>{rowData[4].value}</span> },
+                    { title: "भाव 6", render: rowData => <span>{rowData[5].value}</span> },
+                    { title: "भाव 7", render: rowData => <span>{rowData[6].value}</span> },
+                    { title: "भाव 8", render: rowData => <span>{rowData[7].value}</span> },
+                    { title: "भाव 9", render: rowData => <span>{rowData[8].value}</span> },
+                    { title: "भाव 10", render: rowData => <span>{rowData[9].value}</span> },
+                    { title: "भाव 11", render: rowData => <span>{rowData[10].value}</span> },
+                    { title: "भाव 12", render: rowData => <span>{rowData[11].value}</span> },
+
+
+                  ]}
+                  data={this.state.drakBhavaData}
+                  title=""
+                  options={{
+                    pageSize: 9,
+                    pageSizeOptions: [],
+                    search: false,
+                    paging: false,
+                  }}
+                />
+              </div>
+            </Paper>
+        </Grid>
+        <Grid item xs ={12}>
+        <Paper className="paper-container">
+              <div className="paper-heading">ग्रहोपरि ग्रहाणां  दृष्टयः </div>
+              <div className="paper-table">
+                <MaterialTable
+                  columns={[
+                    { title: "सूर्य", render: rowData => <span>{rowData[0].value}</span> },
+                    { title: "चन्द्र", render: rowData => <span>{rowData[1].value}</span> },
+                    { title: "मंगल", render: rowData => <span>{rowData[2].value}</span> },
+                    { title: "बुध", render: rowData => <span>{rowData[3].value}</span> },
+                    { title: "गुरु", render: rowData => <span>{rowData[4].value}</span> },
+                    { title: "शुक्र", render: rowData => <span>{rowData[5].value}</span> },
+                    { title: "शनि", render: rowData => <span>{rowData[6].value}</span> },
+                    { title: "राहु", render: rowData => <span>{rowData[7].value}</span> },
+                    { title: "केतु", render: rowData => <span>{rowData[8].value}</span> },
+
+                  ]}
+                  data={this.state.drakPlanetData}
+                  title=""
+                  options={{
+                    pageSize: 9,
+                    pageSizeOptions: [],
+                    search: false,
+                    paging: false,
+                  }}
+                />
+              </div>
+            </Paper>
+        </Grid>
         </Grid>
       </div>
     );
