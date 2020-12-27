c6962c24cc83764fa8146ff14fb05f6c39a69ccc
tarun trivedi <tarundtrivedi@gmail.com>
2020-12-27 16:44:50 +0530

    Updates

---
+++ precache-manifest.5d2d1f156b256bace2972e6df8858ade.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67afaf9bc3e579b55b71c3b4cfbe84f",
+    "url": "/ProjectAstro/index.html"
+  },
+  {
+    "revision": "9924f0f8b3bc51555f67",
+    "url": "/ProjectAstro/static/css/main.d000826e.chunk.css"
+  },
+  {
+    "revision": "b026abed84c585951bb1",
+    "url": "/ProjectAstro/static/js/2.3f3a99c9.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/ProjectAstro/static/js/2.3f3a99c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea8f5ee4dc7cacb8af51",
+    "url": "/ProjectAstro/static/js/3.c5ff0b2b.chunk.js"
+  },
+  {
+    "revision": "fd77f13855b5676a8e117c5b6583996c",
+    "url": "/ProjectAstro/static/js/3.c5ff0b2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7587ee98e7d807adfd",
+    "url": "/ProjectAstro/static/js/4.de6387ab.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/ProjectAstro/static/js/4.de6387ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05677791832b22a9d23c",
+    "url": "/ProjectAstro/static/js/5.d775475a.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/ProjectAstro/static/js/5.d775475a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9924f0f8b3bc51555f67",
+    "url": "/ProjectAstro/static/js/main.95461714.chunk.js"
+  },
+  {
+    "revision": "4b6ed94ec97da94df34b",
+    "url": "/ProjectAstro/static/js/runtime-main.167ea748.js"
+  }
+]);
