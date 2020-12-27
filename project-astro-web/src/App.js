b1b421b6bacf6e9aa3e92e01cfaa3c0a488615cd
tarun trivedi <tarundtrivedi@gmail.com>
2020-12-27 16:36:31 +0530

    routes update

--- project-astro-web/src/App.js
+++ project-astro-web/src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/data-entry" component={DataEntry} />
+            <Route exact path="/" component={DataEntry} />
+            {/* <Route exact path="/data-entry" component={DataEntry} /> */}
           </Switch>
     </div>
   );
